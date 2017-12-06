65e5fdd6ea157e897c9fe4b20257f3c430552b88
Vincent Ramdhanie <vramdhanie@gmail.com>
2017-12-06 16:12:42 -0400

    JavaScript This keyword Tutorial

---
+++ README.md
@@ -0,0 +1,3 @@
+# This Keyword
+_A tutorial_
+
---
+++ index.js
@@ -0,0 +1,10 @@
+/* Welcome, this tutorial walks through the meaning of the "this" keyword
+ * in JavaScript
+ *
+ * **** TOP LEVEL
+ * At the top level in the browser this refers to the window object.
+ */
+
+console.log(this === window);
+
+
